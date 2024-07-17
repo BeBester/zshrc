af92e6322dd79904298358d9a4a6b64da2490358
fei <zhupengfei.k@gmail.com>
2024-07-18 01:35:52 +0800

    新增minicoda

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,13 +1,13 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/local/anaconda3/bin/conda' 'shell.zsh' 'hook' 2>/dev/null)"
+__conda_setup="$('/home/fei/miniconda3/bin/conda' 'shell.zsh' 'hook' 2>/dev/null)"
 if [ $? -eq 0 ]; then
 	eval "$__conda_setup"
 else
-	if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
-		. "/usr/local/anaconda3/etc/profile.d/conda.sh"
+	if [ -f "/home/fei/miniconda3/etc/profile.d/conda.sh" ]; then
+		. "/home/fei/miniconda3/etc/profile.d/conda.sh"
 	else
-		export PATH="/usr/local/anaconda3/bin:$PATH"
+		export PATH="/home/fei/miniconda3/bin:$PATH"
 	fi
 fi
 unset __conda_setup
